baac7c5d176673bf6dd947a3a0bbb49752d86054
webmasterdevlin <webmasterdevlin@gmail.com>
2017-11-26 20:04:46 +0800

    Add project files.

---
+++ SignaturePadXfDemo/SignaturePadXfDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignaturePad.Forms;
+using Xamarin.Forms;
+
+namespace SignaturePadXfDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Button_OnClicked(object sender, EventArgs e)
+        {
+            FooSignaturePad.Clear();
+        }
+
+        private async void Submit_Button_OnClicked(object sender, EventArgs e)
+        {
+            var signedImageStream = await FooSignaturePad.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+            //TODO: Your logic here
+            await DisplayAlert("Acme Company", "Your signed agreement has been sent", "OK");
+        }
+    }
+}
